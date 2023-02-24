fd21d82506d125e359344d511b4896f227b44451
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2023-02-24 21:44:57 +0100

    Finished with search bar and now I'm Working on pagination from ReactPaginate components

--- src/App.js
+++ src/App.js
@@ -11,14 +11,17 @@ import "bootstrap/dist/js/bootstrap"
 import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
+import styles from './App.scss'
 
 function App() {
   
   let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState('');
   let [fetchData, updateFetchData] = useState([]);
   let {info, results} = fetchData;
 
-  let api =  `https://rickandmortyapi.com/api/character?page=${pageNumber}`;
+  let api =  `https://rickandmortyapi.com/api/character?page=${pageNumber}&name=${search}`;
 
 
   useEffect(() =>{
@@ -36,6 +39,7 @@ function App() {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary" >App</span>
       </h1>
+      <Search setSearch={setSearch} setPageNumber={setPageNumber} />
       <div className="container" >
         <div className="row" >
           <div className="col-3" >
@@ -53,7 +57,10 @@ function App() {
         </div>
 
       </div>
-      <Pagination setPageNumber={setPageNumber}/>
+      <Pagination 
+      info={info} 
+      pageNumber={pageNumber} 
+      setPageNumber={setPageNumber}/>
     </div>
   );
 }
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
+import ReactPaginate from 'react-paginate';
 
-const Pagination = ({ setPageNumber }) => {
-    let next = ()=>{
+const Pagination = ({ info, setPageNumber, pageNumber }) => {
+    /*let next = ()=>{
         setPageNumber((x) =>x+1)
     };
     let prev = ()=>{
+      if(pageNumber === 1) return;
         setPageNumber((x) =>x-1)
-    };
+    };*/
   return (
-    <div className='container d-flex justify-content-center gap-5 my-5'> 
+
+    <ReactPaginate pageCount={info.pages}/>
+    /*<div className='container d-flex justify-content-center gap-5 my-5'> 
     <button type="button" onClick={prev} className='btn btn-primary' >Previous</button> 
     <button type="button" onClick={next} className='btn btn-primary' >Next</button>
-    </div>
+    </div>*/
   )
 }
 
