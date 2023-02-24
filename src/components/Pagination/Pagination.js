65ae713222b38482a1a4fef1a3d56018e8e0ffb5
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2023-02-24 22:33:07 +0100

    finished wirh ReactPaginate

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
+import styles from './Pagination.module.scss'
 
 const Pagination = ({ info, setPageNumber, pageNumber }) => {
     /*let next = ()=>{
@@ -11,7 +12,21 @@ const Pagination = ({ info, setPageNumber, pageNumber }) => {
     };*/
   return (
 
-    <ReactPaginate pageCount={info.pages}/>
+    <ReactPaginate 
+    className={`pagination justify-content-center gap-4 my-4 text-decoration-none text-pagination `}
+    nextLabel='Next'
+    nextClassName={`btn btn-primary ${styles.text}`}
+    previousLinkClassName={` ${styles.text}`}
+    nextLinkClassName={` ${styles.text}`}
+    previousLabel='Prev'
+    previousClassName={`btn btn-primary ${styles.text}`}
+    pageClassName='page-item'
+    pageLinkClassName='page-link'
+    activeClassName='active'
+    forcePage={pageNumber=== 1 ? 0: pageNumber - 1}
+    onPageChange={(data)=>{setPageNumber(data.selected + 1)}}
+    pageCount={info?.pages}
+    />
     /*<div className='container d-flex justify-content-center gap-5 my-5'> 
     <button type="button" onClick={prev} className='btn btn-primary' >Previous</button> 
     <button type="button" onClick={next} className='btn btn-primary' >Next</button>
