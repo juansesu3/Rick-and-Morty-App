b01b1fda7df83c1226cb4e02702a98fbb7686d68
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2023-02-24 16:00:42 +0100

    finish with fetching data from API and start with paginatio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React, { useEffect, useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 
 function App() {
+  
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchData, updateFetchData] = useState([]);
+  let {info, results} = fetchData;
+
+  let api =  `https://rickandmortyapi.com/api/character?page=${pageNumber}`;
+
+
+  useEffect(() =>{
+   
+    (async()=>{
+      let data = await fetch(api).then(res=>res.json());   
+      updateFetchData(data);
+    })();
+},[api]);
+
+
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
@@ -14,7 +34,11 @@ function App() {
             <Filters/>
           </div>
           <div className="col-8" >
-          <div className="row" ></div>
+            <div className="row" >
+              <Cards results={results} />
+             
+              
+            </div>
           </div>
            
 
