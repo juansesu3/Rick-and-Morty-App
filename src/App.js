503550054fbd5f9304ae07a2b16eacca641a6c2a
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2023-02-25 22:34:22 +0100

    Working on routes and navbar styles

--- src/App.js
+++ src/App.js
@@ -13,8 +13,30 @@ import Filters from "./components/Filters/Filters";
 import Pagination from './components/Pagination/Pagination';
 import Search from './components/Search/Search';
 import styles from './App.scss'
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Episodes from './Pages/Episodes';
+import Location from './Pages/Location';
+
+function App(){
+  return(
+
+    <Router>
+      <div className='App' >
+      <Navbar />
+      </div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/episodes' element={<Episodes/>} />
+        <Route path='/location' element={<Location/>} />
+      </Routes>
+    </Router>
+
+  )
+}
+
+const Home=()=> {
 
   let [pageNumber, setPageNumber] = useState(1);
   let [search, setSearch] = useState('');
@@ -35,9 +57,8 @@ function App() {
 
   return (
     <div className="App" >
-      <h1 className="text-center ubuntu my-4 text-light">
-        Rick & Morty <span className="text-primary" >App</span>
-      </h1>
+      
+      
       <Search setSearch={setSearch} setPageNumber={setPageNumber} />
       <div className="container" >
         <div className="row" >
